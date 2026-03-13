58bf87154a04f68eb1f28b3732a8952b69bac258
= <=>
2026-03-13 21:29:36 +0530

    Fix Render port issue

--- symptoms_based_disease_recognition/symptoms_based_disease_recognition/src/app.py
+++ symptoms_based_disease_recognition/symptoms_based_disease_recognition/src/app.py
@@ -337,4 +337,5 @@ def logout():
 # =============================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
